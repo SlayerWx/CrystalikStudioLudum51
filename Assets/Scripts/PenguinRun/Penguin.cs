b8fd9717bd56891723691f4b12bbfbdc16df062c
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-10-03 11:23:27 -0300

    penguin player sprites and anim

--- Assets/Scripts/PenguinRun/Penguin.cs
+++ Assets/Scripts/PenguinRun/Penguin.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Penguin : MonoBehaviour
 {
 
@@ -14,7 +14,9 @@ public class Penguin : MonoBehaviour
     [Space, SerializeField] private float _distanceGround;
 
     [SerializeField] private bool _isGrounded;
-
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] Transform _groundCheck;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
@@ -33,13 +35,21 @@ public class Penguin : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float _speed;
         _rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * _movementSpeed, _rigidbody.velocity.y);
+        _speed = _rigidbody.velocity.x;
+        if (_speed > 0f)
+            _spriteRenderer.flipX = false;
+        else if(_speed < 0f)
+            _spriteRenderer.flipX = true;
+
+        if(_speed != _animator.GetFloat("speed")) _animator.SetFloat("speed", _rigidbody.velocity.x);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         float collisionTopPosY = collision.transform.position.y + (collision.transform.localScale.y / 2f);
-        float posYPlayer = transform.position.y - (transform.localScale.y / 4f);
+        float posYPlayer = _groundCheck.position.y;
 
         _isGrounded = (posYPlayer >= collisionTopPosY);
     }
