c02faf462dfcac79f74f306b2553c0530c3f3f45
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-10-03 12:49:36 -0300

    comic now disappear with changes in the alpha

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SplashScreen : MonoBehaviour
 {
     private static bool firstMainMenu = true;
     public float timer = 4f;
+    public RawImage img;
     private void Start()
     {
         if (firstMainMenu) StartCoroutine(ShowComic());
@@ -18,6 +19,14 @@ public class SplashScreen : MonoBehaviour
         firstMainMenu = false;
         gameObject.SetActive(true);
         yield return new WaitForSeconds(timer);
+        float aux = img.color.a;
+        while (img.color.a > 0)
+        {
+            yield return null;
+            img.color = new Vector4(img.color.r, img.color.g, img.color.b, img.color.a - (Time.deltaTime * 0.5f));
+        }
+
+        img.color = new Vector4(img.color.r, img.color.g, img.color.b, aux);
         gameObject.SetActive(false);
     }
 }
